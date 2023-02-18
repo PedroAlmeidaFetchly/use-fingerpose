ca6f4e39e8d312f689becba9af5415e582616e75
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2023-02-18 20:51:57 -0300

    create useFingerpose hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import useFingerpose from "./hooks/useFingerpose";
+
 function App() {
+  const { loading } = useFingerpose();
+
+  console.log(loading);
   return (
     <div>
       <h1>useFingerpose</h1>
