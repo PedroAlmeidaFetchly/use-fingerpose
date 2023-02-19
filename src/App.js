8b2eae88caee0904ad19686e0635e1c8db7ccdfd
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2023-02-18 21:55:49 -0300

    passin webcam ref to useFingerpose

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useRef } from "react";
 import useFingerpose from "./hooks/useFingerpose";
+import Webcam from "react-webcam";
 
 function App() {
-  const { loading, error } = useFingerpose();
+  const webcamRef = useRef(null);
+  const { handposeLoading, error, cameraLoading } = useFingerpose({
+    timeFrame: 100,
+    webcamRef,
+  });
 
-  console.log(loading, error);
+  console.log(handposeLoading, error, cameraLoading);
   return (
     <div>
       <h1>useFingerpose</h1>
+      <Webcam ref={webcamRef} />
     </div>
   );
 }
--- src/hooks/useFingerpose.js
+++ src/hooks/useFingerpose.js
@@ -2,14 +2,32 @@ import { useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 
-const useFingerpose = () => {
-  const [loading, setLoading] = useState(true);
+const useFingerpose = (config) => {
+  const { timeFrame, webcamRef } = config;
+  const [handposeLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [cameraLoading, setCameraLoading] = useState(true);
 
+  const detect = () => {
+    if (
+      typeof webcamRef.current === "undefined" ||
+      webcamRef.current === null ||
+      webcamRef.current.readyState === 4
+    )
+      return;
+    else {
+      setCameraLoading(false);
+      console.log(webcamRef.current);
+    }
+  };
   const runHandPose = async () => {
     try {
       const net = await handpose.load();
       setLoading(false);
+
+      setInterval(() => {
+        detect(net);
+      }, timeFrame);
     } catch (error) {
       setLoading(false);
       setError(error);
@@ -18,7 +36,7 @@ const useFingerpose = () => {
 
   runHandPose();
 
-  return { loading, error };
+  return { handposeLoading, error, cameraLoading };
 };
 
 export default useFingerpose;
